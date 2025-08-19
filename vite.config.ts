264cc24c2985a50ca21bd6b9e05668c73fdd5430
indoboyz1357 <119711027+indoboyz1357@users.noreply.github.com>
2025-08-19 09:34:45 +0000

    setup vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // penting untuk Vercel/Netlify (jangan './')
+  base: '/', // penting untuk Vercel, jangan './'
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react'], // opsional, biar gak error build
   },
 })
