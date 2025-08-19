4c4c653ce6008fd0e7e42f9f16ed4995d754e153
indoboyz1357 <119711027+indoboyz1357@users.noreply.github.com>
2025-08-19 03:09:22 +0000

    fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // penting untuk deployment ke Vercel/Netlify
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
